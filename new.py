da035e847cf94a22d138d3e755f33b023177e744
contradeepak <deepaktomss@gmail.com>
2025-08-05 12:09:47 +0530

    first commit

---
+++ new.py
@@ -0,0 +1,30 @@
+# printing empty print function
+
+print()
+
+# printing some data in print function
+print("some data")
+# printing more than one data in print function
+print("some data", "some data")
+# Example for sep in print function
+day = 10
+mon = 12
+year = 2027
+print(day,mon,year)
+print(day,mon,year, sep="")
+print(day,mon,year, sep="/")
+print(day,mon,year, sep="_")
+
+
+
+
+# Example for end  = "\n"
+print(1)
+print(2,3)
+
+
+print(1, end="")
+print(2,3)
+
+print(1, end='\n')
+print(2,3)
